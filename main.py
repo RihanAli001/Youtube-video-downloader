1e4a8f622a2478110df4244e8c4a34635f537ddc
Parvej Ali <Parvej.tca1909056@tmu.ac.in>
2022-03-22 19:43:07 +0530

    fix downloading error

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 import sys
 import urllib.request
+import threading
 from Downloader import Ui_MainWindow
 from PyQt5 import QtGui
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QTimer, Qt, QPoint, QUrl
+from PyQt5.QtCore import QTimer, QUrl
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QFileSystemModel, QShortcut
 from pytube import YouTube, Search
 import vlc
@@ -13,10 +14,14 @@ WINDOW_SIZE = 0
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
+    """
+    Initialization function
+    """
+
     def __init__(self, parent=None):
         super(MyMainWindow, self).__init__(parent)
         self.setupUi(self)
-        self.setWindowFlags(Qt.FramelessWindowHint)
+        self.top_bar.hide()
 
         self.instance = vlc.Instance("prefer-insecure")
         self.media_player = self.instance.media_player_new()
@@ -47,6 +52,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.download_btn.clicked.connect(self.download_video)
         QShortcut('Ctrl+D', self).activated.connect(self.download_video)
         self.webview.urlChanged.connect(self.display_webview_url)
+        self.yt = None
 
         self.model = QFileSystemModel()
         self.open_folder_btn.clicked.connect(self.open_folder)
@@ -55,21 +61,144 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
         self.old_pos = self.pos()
         self.option_btn.clicked.connect(self.menu_toggle)
-        self.home_btn.clicked.connect(self.home_menu_page)
-        self.local_btn.clicked.connect(self.local_menu_page)
+        self.home_btn.clicked.connect(self.set_main_home_page)
+        self.local_btn.clicked.connect(self.set_main_local_page)
         self.info_drop_down_btn.clicked.connect(self.info_drop_down)
 
         self.close_btn.clicked.connect(lambda: self.close())
         self.minimize_btn.clicked.connect(lambda: self.showMinimized())
         self.restore_btn.clicked.connect(lambda: self.restore_or_maximize_window())
 
-    def display_webview_url(self):
-        url = str(self.webview.url())
-        url = url.replace("PyQt5.QtCore.QUrl('", "")
+    """
+    Showing short process message to user
+    """
+
+    def short_msg_notification(self, msg):
+        self.short_msg_label.setText(msg)
+
+    """
+    Searching youtube video using video Url.
+    """
+
+    def url_video_search(self):
+        if not self.is_internet():
+            return
+        url = self.search_bar.text()
+        if not len(url):
+            url = "https://youtu.be/Az-mGR-CehY"
+        self.set_main_home_page()
+        try:
+            self.yt = YouTube(url, on_progress_callback=self.download_callback)
+        except:
+            print("URL is not valid...")
+            self.short_msg_notification("URL is not valid...")
+            self.query_video_search()
+            return
+        self.web_update_video()
+
+    """
+    Searching youtube video using search query.
+    """
+
+    def query_video_search(self):
+        if not self.is_internet():
+            return
+        query = self.search_bar.text()
+        if not len(query):
+            query = "Rihan Ali001"
+        self.set_main_home_page()
+        try:
+            s = Search(query)
+        except:
+            print("Query search error...")
+            return
+        print("Query Search:", query)
+        self.yt = s.results[0]
+        self.web_update_video()
+
+    """
+    Checking internet connection for playing youtube videos.
+    """
+
+    def is_internet(self):
+        try:
+            urllib.request.urlopen("http://youtube.com")
+            return True
+        except:
+            print("Internet is not connected")
+            self.short_msg_notification("Internet is not connected...")
+            return False
+
+    """
+    Updating home page for new video Url.
+    """
+
+    def web_update_video(self):
+        self.webview.setUrl(QUrl(f'{self.yt.watch_url}?rel=0'))
+        print(f'{self.yt.watch_url}?rel=0')
+
+    """
+    Displaying youtube video url
+    """
+
+    def display_webview_url(self, url):
+        url = str(url).replace("PyQt5.QtCore.QUrl('", "")
         url = url.replace("')", "")
         print(url)
-        yt = YouTube(url)
-        self.video_info_update(yt)
+        self.yt = YouTube(url, on_progress_callback=self.download_callback)
+        self.update_video_qualities()
+        self.video_info_update()
+
+    """
+    Give video quality options to user for downloading video
+    """
+
+    def update_video_qualities(self):
+        ...
+
+    def download_callback(self, stream, chunk, bytes_remaining):
+        file_size = stream.filesize
+        bytes_downloaded = file_size - bytes_remaining
+        self.short_msg_notification(f"{bytes_downloaded}/{file_size} Downloading...")
+        if bytes_downloaded == file_size:
+            self.short_msg_notification(f"{bytes_downloaded}/{file_size} Downloaded")
+
+    """
+    Downloading youtube video with selected video resolution.
+    """
+
+    def download_video(self):
+        self.short_msg_notification("Wait, downloading...")
+        threading.Thread(target=self.download, args=()).start()
+
+    def download(self):
+        self.yt.streams.filter(progressive=True).desc().first().download()
+
+
+    """"
+    Youtube video information update in video information section
+    """
+
+    def video_info_update(self):
+        if self.yt.title != "":
+            self.video_title_field.setText(self.yt.title)
+            print("Video title " + self.yt.title + "...")
+        if self.yt.description != "":
+            self.video_description_field.setText(self.yt.description)
+            print("Video description " + self.yt.description[:40] + "...")
+        video_len = self.yt.length
+        hours = int(video_len / (60 * 60))
+        minutes = int((video_len - hours * 3600) / 60)
+        seconds = video_len - hours * 3600 - minutes * 60
+        self.video_length_field.setText(str(hours) + ":" + str(minutes) + ":" + str(seconds))
+        print("Video length " + str(self.yt.length) + "...")
+        if self.yt.author != "":
+            self.video_author_field.setText(self.yt.author)
+            print("Video author " + self.yt.author + "...")
+
+    """
+    Restore or maximizing the window
+    """
 
     def restore_or_maximize_window(self):
         global WINDOW_SIZE
@@ -85,19 +214,47 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             icon.addPixmap(QtGui.QPixmap(":/icons_white/icons_White/icon_maximize.png"))
         self.restore_btn.setIcon(icon)
 
-    def mouse_press_event(self, event):
-        self.old_pos = event.globalPos()
-
-    def mouse_move_event(self, event):
-        delta = QPoint(event.globalPos() - self.old_pos)
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self.old_pos = event.globalPos()
+    """
+    Show/hide left navigation bar.
+    """
 
     def menu_toggle(self):
-        if self.extra_left_space.width() == 0:
+        if self.left_nav.minimumWidth() == 0:
+            self.left_nav.setMinimumWidth(60)
+            self.left_nav.setMaximumWidth(60)
+        else:
+            self.left_nav.setMinimumWidth(0)
+            self.left_nav.setMaximumWidth(0)
+
+    """
+    Showing main home page (Youtube website page) and hide media player page to the user.
+    """
+
+    def set_main_home_page(self):
+        if self.media_player.is_playing():
+            self.play_pause()
+
+        if self.extra_left_space.minimumWidth() != 0:
+            self.extra_left_space.setMinimumWidth(0)
+
+        if self.main_stacked_pages.currentWidget() == self.main_local_page:
+            self.main_stacked_pages.setCurrentWidget(self.main_home_page)
+
+    """
+    Showing main local page (media player page) and hide main home page to user.
+    """
+
+    def set_main_local_page(self):
+        if self.main_stacked_pages.currentWidget() == self.main_home_page:
+            self.main_stacked_pages.setCurrentWidget(self.main_local_page)
+        elif self.extra_left_space.minimumWidth() == 0:
             self.extra_left_space.setMinimumWidth(300)
         else:
             self.extra_left_space.setMinimumWidth(0)
+
+    """
+    Show/hide video information page to the user.
+    """
 
     def info_drop_down(self):
         icon = QtGui.QIcon()
@@ -109,19 +266,23 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             icon.addPixmap(QtGui.QPixmap(":/icons_white/icons_White/cil-arrow-top.png"))
         self.info_drop_down_btn.setIcon(icon)
 
-    def home_menu_page(self):
-        if self.stackedWidget.currentWidget() == self.home_page:
-            self.menu_toggle()
-        elif self.extra_left_space.width() == 0:
-            self.menu_toggle()
-        self.stackedWidget.setCurrentWidget(self.home_page)
+    """
+    Showing youtube video information.
+    """
 
-    def local_menu_page(self):
-        if self.stackedWidget.currentWidget() == self.local_page:
-            self.menu_toggle()
-        elif self.extra_left_space.width() == 0:
-            self.menu_toggle()
-        self.stackedWidget.setCurrentWidget(self.local_page)
+    def show_yt_video_information(self):
+        print(self.webview.title())
+
+    """
+    Showing media player video information.
+    """
+
+    def show_local_video_information(self):
+        print(self.media.get_meta(0))
+
+    """
+    Play/pause the media player if player has no media then open file
+    """
 
     def play_pause(self):
         if self.media_player.is_playing():
@@ -136,91 +297,20 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.play_pause_btn.setText("Pause")
             self.timer.start()
             self.isPaused = False
-            if self.video_player_frame.maximumHeight() == 0:
-                self.web_video_player_frame.setMaximumHeight(0)
-                self.video_player_frame.setMaximumHeight(16777215)
+            if self.main_stacked_pages.currentWidget() == self.main_home_page:
+                self.main_stacked_pages.setCurrentWidget(self.main_local_page)
+
+    """
+    Stop media player.
+    """
 
     def stop(self):
         self.media_player.stop()
         self.play_pause_btn.setText("Play")
 
-    def show_yt_video_information(self):
-        print(self.webview.title())
-
-    def show_local_video_information(self):
-        print(self.media.get_meta(0))
-
-    def download_video(self):
-        print("Downloading video with", self.download_video_quality.currentText(), "quality...")
-
-    @staticmethod
-    def is_internet():
-        try:
-            urllib.request.urlopen("http://youtube.com")
-            return True
-        except:
-            print("Internet is not connected")
-            return False
-
-    def web_view_update(self):
-        if self.media_player.is_playing():
-            self.play_pause()
-        if self.web_video_player_frame.maximumHeight() == 0:
-            self.web_video_player_frame.setMaximumHeight(16777215)
-            self.video_player_frame.setMaximumHeight(0)
-
-    def url_video_search(self):
-        if not self.is_internet():
-            return
-        url = self.search_bar.text()
-        if not len(url):
-            url = "https://youtu.be/Az-mGR-CehY"
-        self.web_view_update()
-        try:
-            yt = YouTube(url)
-        except:
-            print("URL is not valid...")
-            self.query_video_search()
-            return
-        self.web_update_video(yt)
-
-    def query_video_search(self):
-        if not self.is_internet():
-            return
-        query = self.search_bar.text()
-        if not len(query):
-            query = "RihanHack"
-        self.web_view_update()
-        try:
-            s = Search(query)
-        except:
-            print("Query search error...")
-            return
-        print("Query Search:", query)
-        yt = s.results[0]
-        self.web_update_video(yt)
-
-    def web_update_video(self, yt):
-        self.webview.setUrl(QUrl(f'{yt.watch_url}?rel=0'))
-        print(f'{yt.watch_url}?rel=0')
-        self.video_info_update(yt)
-
-    def video_info_update(self, yt):
-        if yt.title != "":
-            self.video_title_field.setText(yt.title)
-            print("Video title " + yt.title + "...")
-        if yt.description != "":
-            self.video_description_field.setText(yt.description)
-            print("Video description " + yt.description[:40] + "...")
-        video_len = yt.length
-        hours = int(video_len / (60 * 60))
-        minutes = int((video_len - hours * 3600) / 60)
-        seconds = video_len - hours * 3600 - minutes * 60
-        self.video_length_field.setText(str(hours)+":"+str(minutes)+":"+str(seconds))
-        print("Video length " + str(yt.length) + "...")
-        if yt.author != "":
-            self.video_author_field.setText(yt.author)
-            print("Video author " + yt.author + "...")
+    """"
+    Open file and set file to media player.
+    """
 
     def open_file(self, filename=None):
         if filename is None:
@@ -235,7 +325,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.media_player.set_media(self.media)
 
         self.media.parse()
-        # self.setWindowTitle(self.media.get_meta(0))
+        self.setWindowTitle("YouTube Video Downloader - " + self.media.get_meta(0))
         self.top_bar_label.setText(self.media.get_meta(0) + " - YouTube Video Downloader")
 
         if sys.platform.startswith('linux'):  # for Linux using the X Server
@@ -247,6 +337,10 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.play_pause()
         self.show_local_video_information()
 
+    """
+    Open folder for displaying videos in navigation section.
+    """
+
     def open_folder(self):
         dir_path = QFileDialog.getExistingDirectory(self, "Select Folder", os.path.abspath('/home'))
         if not dir_path:
@@ -256,26 +350,50 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.tree_view_local.setRootIndex(self.model.index(dir_path))
         self.tree_view_local.setColumnWidth(0, 250)
 
+    """
+    Set file to media player from local navigation section.
+    """
+
     def set_file_to_player(self):
         if not self.model.isDir(self.tree_view_local.currentIndex()):
             self.open_file(self.model.filePath(self.tree_view_local.currentIndex()))
 
+    """
+    Set media player volume.
+    """
+
     def set_volume(self, volume):
         self.media_player.audio_set_volume(volume)
+
+    """
+    Increase the media player volume.
+    """
 
     def update_volume_up(self):
         self.media_player.audio_set_volume(self.media_player.audio_get_volume() + 1)
         print(self.media_player.audio_get_volume())
 
+    """
+    Decrease the media player volume.
+    """
+
     def update_volume_down(self):
         self.media_player.audio_set_volume(self.media_player.audio_get_volume() - 1)
         print(self.media_player.audio_get_volume())
+
+    """
+    Set media player slider position.
+    """
 
     def set_position(self, position):
         try:
             self.media_player.set_position(position / 1000.0)
         finally:
             print("Position calculation error")
+
+    """
+    Update media player Ui when it is playing.
+    """
 
     def update_ui(self):
         try:
